df06b49962d6b3ebfc0af5425c3cd9637abc06bf
venkyabajal@gmail.com <venkyabajal@gmail.com>
2016-01-12 17:55:52 +0000

    Validation Added

--- database_setup.py
+++ database_setup.py
@@ -7,34 +7,24 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
  
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
-   
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    menuItems = relationship("MenuItem",cascade = "all, delete-orphan")
- 
-class MenuItem(Base):
-    __tablename__ = 'menu_item'
-
-
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
-    description = Column(String(250))
-    price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant) 
+DATABASE_URL = 'postgres://ytrvdxrnzzphvf:dfjwYJ8qoE859jy9MVvYsRSd9v@ec2-54-83-52-71.compute-1.amazonaws.com:5432/des6sj4shuk7v4'
 
 class Person(Base):
     __tablename__ = 'person'
 
 
     name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
-    
+    phone = Column(String(10), primary_key = True)
+    interests = Column(String(500), nullable = False)
 
-engine = create_engine('postgres://ytrvdxrnzzphvf:dfjwYJ8qoE859jy9MVvYsRSd9v@ec2-54-83-52-71.compute-1.amazonaws.com:5432/des6sj4shuk7v4')
- 
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.phone,
+            'interests': self.interests
+        }
 
+engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(engine)
--- project.py
+++ project.py
@@ -3,9 +3,9 @@ app = Flask(__name__)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem,Person
+from database_setup import Base, DATABASE_URL,Person
 
-engine = create_engine('postgres://ytrvdxrnzzphvf:dfjwYJ8qoE859jy9MVvYsRSd9v@ec2-54-83-52-71.compute-1.amazonaws.com:5432/des6sj4shuk7v4')
+engine = create_engine(DATABASE_URL)
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -15,64 +15,56 @@ session = DBSession()
 def home():
 	return render_template('home.html')
 
-@app.route('/restaurants/<int:restaurant_id>/')
-def restaurantMenu(restaurant_id = 1):
-  restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-  items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-  return render_template('templates.html', restaurant=restaurant, items = items)
-  
-#Task 1: Create route for newMenuItem function here
-@app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET','POST'])
-def newMenuItem(restaurant_id):
-  if request.method == 'POST':
-  	newItem = MenuItem(name = request.form['name'],restaurant_id=restaurant_id)
-  	session.add(newItem)
-  	session.commit()
-	flash("Item Created")
-  	return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
-  else:
-  	return render_template('newitem.html',restaurant_id = restaurant_id)
-  
-#Task 2: Create route for editMenuItem function here
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/',methods=['GET','POST'])
-def editMenuItem(restaurant_id, menu_id):
-  editedItem = session.query(MenuItem).filter_by(id = menu_id).one()
-  if request.method == 'POST':
-	if request.form['name']:
-		editedItem.name = request.form['name']
-		session.add(editedItem)
-		session.commit()
-		flash("Item edited")
-		return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
-  else:
-#USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
-		return render_template('edititem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = editedItem)
-  
-#Task 3: Create a route for deleteMenuItem function here
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/',methods=['GET','POST'])
-def deleteMenuItem(restaurant_id, menu_id):
-	editedItem = session.query(MenuItem).filter_by(id = menu_id).one()
-	if request.method == 'POST':
-		session.delete(editedItem)
-		session.commit()
-		flash("Item Deleted")
-		return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+
+@app.route('/person/add/<int:phone>/<string:name>/',methods=['GET','POST'])
+def addPerson(phone,name): 
+	if request.method == "GET":
+		newItem = Person(id = phone,name=name)
+  		session.add(newItem)
+  		session.commit()
+		return jsonify(Status="get added")
+
+@app.route('/person/add/',methods=['POST'])
+def addPersonPost():
+
+	if request.headers["phone"] and request.headers["phone"] != "" and  len(request.headers["phone"])==10:
+		phone = request.headers["phone"]
 	else:
-	#USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
-		return render_template('deleteitem.html', restaurant_id = restaurant_id, item = editedItem)
+		return jsonify(status="failed",reason="Phone Number Incorrect")
 
+	if request.headers["name"] and request.headers["name"] != "":
+		name  = request.headers["name"]
+	else:
+		return jsonify(status="failed",reason="Name Incorrect")
 
-@app.route('/person/add/<int:personid>/<string:name>')
-def addPerson(personid,name): 
-	newItem = Person(id = personid,name=name)
-  	session.add(newItem)
-  	session.commit()
-	return jsonify(status="Added")
+	if request.headers["interests"] and request.headers["interests"] != "":
+		interests  = request.headers["interests"]
+	else:
+		interests = ""
 
+	person = session.query(Person).filter_by(id = phone).one()
+	if person:
+		return jsonify(status="failed",reason="Already Exists")
 
-  
-  
+	newItem = Person(phone = phone,name=name,interests = interests)
+	session.add(newItem)
+	session.commit()
+	return jsonify(status="success")
 
-app.debug = True
-app.secret_key = "kanilamba"
-#app.run(host = '0.0.0.0', port = 5000)
+@app.route('/person/view/',methods=['POST'])
+def viewPersonPost():
+	if request.headers["phone"] and request.headers["phone"] != "" and  len(request.headers["phone"])!=10:
+		phone = request.headers["phone"]
+	else:
+		return jsonify(status="failed",reason="Phone Number Incorrect")
+
+	person = session.query(Person).filter_by(id = phone).one()
+	if person:
+		return jsonify(status = "success",data = person)
+	else:
+		return jsonify(status = "success",reason="Not Found")
+
+if __name__ == '__main__':
+	app.debug = True
+	app.secret_key = "kanilamba"
+	app.run(host = '0.0.0.0', port = 5000)
